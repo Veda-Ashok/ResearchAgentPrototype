4222159fd4de893eeb44442f13819395ce133586
Veda Ashok <vashok061@gmail.com>
2020-11-10 20:52:46 -0800

    Secodn version of agent. random policy picker not working

---
+++ agent.py
@@ -0,0 +1,91 @@
+import random
+acclimation_queue = []#tracks windows
+sub_policies = [0, 1, 2, 3 ,4] #mock set of policies
+
+# pick a policy
+# get the first score
+# if queue is not window size, add to queue
+# if queue is window size, pop and push
+
+#pick a random policy
+def pick_policy():
+    current_policy = random.choice(sub_policies)
+    print('policy: ', current_policy)
+    return current_policy
+
+pick_policy() #pick starting policy
+
+# track moving window
+def store_score(c, p, w):
+    if len(acclimation_queue) is w :
+        acclimation_queue.pop(0)
+    acclimation_queue.append(p/c)
+    return acclimation_queue
+    
+
+# check if acclimated
+def is_acclimated(q):
+    acclimated = True
+    if len(q) is 1:
+        acclimated = False
+    for i in range(len(q)):
+        if q[0] != q[i]:
+            acclimated = False
+        else:
+            current_policy = pick_policy()
+    return acclimated
+
+
+
+print(store_score(10, 9, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+print(store_score(10, 4, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+print(store_score(10, 3, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+
+print(store_score(10, 6, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+print(store_score(10, 8, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+print(store_score(10, 9, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+print(store_score(10, 9, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+print(store_score(10, 9, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+print(store_score(10, 9, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+print(store_score(10, 9, 5))
+print(is_acclimated(acclimation_queue))
+print('\n')
+
+
+
+
+
