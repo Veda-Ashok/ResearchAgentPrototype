a992aaea4f4bd0e071e400e120445ff7437a127a
Veda Ashok <vashok061@gmail.com>
2020-11-15 20:45:54 -0800

    Global vs local variable issue with current_policy. Implemented list of tuples to track progress.

--- agent.py
+++ agent.py
@@ -2,23 +2,8 @@ import random
 import statistics
 acclimation_queue = []#tracks windows
 performance_list = []
+trajectory = []
 sub_policies = [0, 1, 2, 3, 4] #mock set of policies
-
-# pick a policy
-# get the first score
-# if queue is not window size, add to queue
-# if queue is window size, pop and push
-# if all scores in queue are consistent, change policy
-
-
-# QUESTIONS:
-# Do we want to check within a variance for each score?
-# How do we want to determine which policy is chosen?
-#     - Have 2 or 3 subpolicies in 3 difficulty levels
-#     - If score is consistent and high, pick policy from one level harder (unless they are already on the difficult level??)
-#     - If score is medium, pick same level
-#     - If score is low, pick lower level (unless they are already on the easy level??)
-
 
 # How their score changes after changing policy
 # How quickly they die
@@ -27,31 +12,19 @@ sub_policies = [0, 1, 2, 3, 4] #mock set of policies
 
 #pick a random policy
 def pick_policy():
-    print('picking policy')
-    # difficulty_level = 0
-    # if len(acclimation_queue) == 0:
-    #     current_policy = random.choice(sub_policies[difficulty_level])
-    # elif acclimation_queue[0] >= 0.8:
-    #     print('difficult')
-    #     difficulty_level = 2
-    # elif acclimation_queue[0] >= 0.5 and acclimation_queue[0] < 0.7:
-    #     print('medium')
-    #     difficulty_level = 1
-    # elif acclimation_queue[0] < 0.5:
-    #     print('easy')
-    #     difficulty_level = 0
-
-    current_policy = random.choice(sub_policies)
-    print('policy: ', current_policy)
+    current_policy = random.choice(sub_policies) 
+    # print('policy: ', policy)
     return current_policy
 
-pick_policy() #pick starting policy
+current_policy = pick_policy() #pick_policy() #pick starting policy
+# print(current_policy)
+
 
 # track moving window
 def store_score(c, p, w):
     score = p/c
-    performance_list.append(score)
-    if len(acclimation_queue) is w :
+    trajectory.append(score)
+    if len(acclimation_queue) is w:
         acclimation_queue.pop(0)
     acclimation_queue.append(score)
     return acclimation_queue
@@ -74,88 +47,138 @@ def is_acclimated(q, w):
         else:
             acclimated = False
             return acclimated
-    if acclimated == True:   
-        pick_policy() 
+    if acclimated == True:
+        performance_list.append((current_policy, trajectory))  
+        # print('traj b4: ', trajectory)
+        # print('current- pol: ', current_policy)
+        trajectory.clear()
+        # print('traj after: ', trajectory)
+        current_policy = pick_policy() 
     return acclimated
 
 
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 9, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 4, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 3, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 6, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 8, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 9, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 9, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 9, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 9, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 9, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 4, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 3, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
+
+print('trajectory: ', trajectory)
 print(store_score(10, 2, 5))
 print(is_acclimated(acclimation_queue, 5))
-print('\n')
-
-print(store_score(10, 3, 5))
-print(is_acclimated(acclimation_queue, 5))
-print('\n')
-
-print(store_score(10, 3, 5))
-print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 3, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 3, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
+print('\n')
+
+print('trajectory: ', trajectory)
+print(store_score(10, 3, 5))
+print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
 
 
+print('trajectory: ', trajectory)
 print(store_score(10, 3, 5))
 print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
+print('\n')
+
+
+print('trajectory: ', trajectory)
+print(store_score(10, 3, 5))
+print(is_acclimated(acclimation_queue, 5))
+print('performance: ', performance_list)
 print('\n')
